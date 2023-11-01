053133b6f2ddc8bc9412363f4013c88e8d296f48
robertnelson-co <hello@robertnelson.co>
2023-11-01 19:43:28 -0400

    completed second activity

--- 2.py
+++ 2.py
@@ -1,1 +1,8 @@
 # Write your solution for algorithm 2 below
+
+numbers_list = [2, 44, 12, 78, 6, 11, 89, 3, 26, 71, 8]
+
+numbers_list_sorted = sorted(numbers_list)
+
+print (numbers_list)
+print (numbers_list_sorted)
