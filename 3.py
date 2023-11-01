634f3ce7b39120bc003d66ffdd912e173b3fbf31
robertnelson-co <hello@robertnelson.co>
2023-11-01 19:44:53 -0400

    compelted third activity

--- 3.py
+++ 3.py
@@ -1,1 +1,8 @@
 # Write your solution for algorithm 3 below
+
+numbers_list = [2, 44, 12, 78, 6, 11, 89, 3, 26, 71, 8]
+
+desc_numbers_list = sorted(numbers_list, reverse=True)
+
+print(numbers_list)
+print(desc_numbers_list)
